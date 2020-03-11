4b6ef6e21cf626949c5fb8c68871512a40b591c4
Fairbanks <Meghan.Fairbanks@transcore.com>
2020-03-11 09:54:56 -0500

    Add regex rules for seletcing text and add the other eff word lists.

--- passGen.py
+++ passGen.py
@@ -1,22 +1,42 @@
 #! python3
 
-import os, random, ctypes
+import os, re, random
 
+#path to wordlist to be used
 filepath = "c:\\pythonScripts\\eff_large_wordlist.txt"
-random1 = random.randint(0,7775)
+
+#count the number of entries in the wordlist
+with open(filepath) as lc:
+    linecount = 0
+    for cnt, line in enumerate(lc):
+        linecount += 1
+
+#randomize which word to select from word list
+random1 = random.randint(0,linecount-1)
+
+#select a random entry from the list
+with open(filepath) as fp:
+    for cnt, line in enumerate(fp):
+        if cnt == random1:
+            select = line
+
+#randomize a two digit number to put after the selected word
 random2 = random.randint(1,99)
 
+#Add a leading zero if the number is less that 10
 if random2 < 10:
     trail = "0" + str(random2)
 else:
     trail = str(random2)
+    
+#Use regex to filter out everything but letters
+#put everything together and capitalize the first letter
+pattern =  ['[a-z]+']
+for p in pattern:
+    match = re.findall(p, select)
+    print(match[0].capitalize() + trail)
+    
 
-with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
-        if cnt == random1:
-            select = line[6:]
-            pword = select.capitalize().rstrip() + str(trail)
-            print(pword)
-            #input()
-            #ctypes.windll.user32.MessageBoxW(0, str(pword), "Password Generator", 1)
+
+
 
