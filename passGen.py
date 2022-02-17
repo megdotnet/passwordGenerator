ddae084c2fe427eb0820f83eee1bba96abe4687a
Meghan Fairbanks <41306593+megdotnet@users.noreply.github.com>
2022-02-17 16:14:10 -0600

    longer passwords, and 10 at a time

--- passGen.py
+++ passGen.py
@@ -1,6 +1,7 @@
 #! python3
 
 import os, re, random
+x = 0
 
 #path to wordlist to be used
 filepath = "c:\\pythonScripts\\eff_large_wordlist.txt"
@@ -11,30 +12,45 @@ with open(filepath) as lc:
     for cnt, line in enumerate(lc):
         linecount += 1
 
-#randomize which word to select from word list
-random1 = random.randint(0,linecount-1)
+def passgen(l):
+    #randomize which word to select from word list
+    random1 = random.randint(0,linecount-1)
 
-#select a random entry from the list
-with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
-        if cnt == random1:
-            select = line
+    #select a random entry from the list
+    with open(filepath) as fp:
+        for cnt, line in enumerate(fp):
+            if cnt == random1:
+                select = line
 
-#randomize a two digit number to put after the selected word
-random2 = random.randint(1,99)
+    #randomize a two digit number to put after the selected word
+    random2 = random.randint(1,99)
 
-#Add a leading zero if the number is less that 10
-if random2 < 10:
-    trail = "0" + str(random2)
-else:
-    trail = str(random2)
+    #Add a leading zero if the number is less that 10
+    if random2 < 10:
+        trail = "0" + str(random2)
+    else:
+        trail = str(random2)
+        
+    #Use regex to filter out everything but letters
+    #put everything together and capitalize the first letter
+    pattern =  ['[a-z]+']
+    for p in pattern:
+        match = re.findall(p, select)
+        password = (match[0].capitalize() + trail)
+        return password
+        
+print(" ")
+
+while x != 10: 
+    password = (passgen(linecount))
+    if len(password) >= 8:
+        print(password)
+        x += 1
     
-#Use regex to filter out everything but letters
-#put everything together and capitalize the first letter
-pattern =  ['[a-z]+']
-for p in pattern:
-    match = re.findall(p, select)
-    print(match[0].capitalize() + trail)
+print(" ")
+
+
+
     
 
 
